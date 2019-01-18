ae540382a336ca4839cdfd426718d57353b1dd26
KennethHung <maple135790@gmail.com>
2019-01-18 16:50:06 +0800

    add is_Prime detail

--- 6kyu/is_Prime.py
+++ 6kyu/is_Prime.py
@@ -1,3 +1,10 @@
+'''
+Define a function isPrime/is_prime() that takes one integer argument 
+and returns true/True or false/False depending on if the integer is a prime.
+Per Wikipedia, 
+a prime number (or a prime) is a natural number greater than 1 
+that has no positive divisors other than 1 and itself.
+'''
 def is_prime(num):
     num =int(num)
     if num ==2:
