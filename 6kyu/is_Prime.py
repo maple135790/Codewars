441efd961dd6b22d32f2a886609eba18918c6a1e
KennethHung <maple135790@gmail.com>
2019-01-18 16:46:41 +0800

    add is_Prime

---
+++ 6kyu/is_Prime.py
@@ -0,0 +1,17 @@
+def is_prime(num):
+    num =int(num)
+    if num ==2:
+        return True
+    end2 =["2","4","6","8"]
+    if str(num)[-1] in end2 or num <2:
+        return False
+    for i in range(2,num):
+        if num % i ==0:
+            return False
+    return True
+if __name__ == "__main__":
+    try:
+        a =input("Number: ")
+        print(is_prime(a))
+    except TypeError as e:
+        print(e)
